72f6309f6b976517dbed9e6b6a76f93b271bcb0d
nicholas-patterson <nicholaspatterson36@gmail.com>
2020-04-04 02:31:36 -0400

    :sparkles: adds index page
    adds index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,60 @@
 import React from "react";
+import { graphql, useStaticQuery } from "gatsby";
 import Layout from "../components/Layout";
+import Navbar from "../components/Navbar";
 import Header from "../components/Header";
 import About from "../components/About";
 import Portfolio from "../components/Portfolio";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
+import "../styles/index.scss";
+import SEO from "../components/SEO";
 
 const Index = () => {
+  const data = useStaticQuery(graphql`
+    query ImageAndbgImage {
+      image: file(relativePath: { eq: "images/FinalDesigncopy.png" }) {
+        id
+        childImageSharp {
+          fixed(width: 150) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+
+      projects: allProjectsJson {
+        edges {
+          node {
+            id
+            title
+            description
+            resources
+            github_link
+            image {
+              childImageSharp {
+                fluid {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+
   return (
-    <Layout>
-      <Header />
-      <About />
-      <Portfolio />
-      <Contact />
-      <Footer />
-    </Layout>
+    <>
+      <Layout>
+        <SEO />
+        <Navbar imageData={data.image.childImageSharp.fixed} />
+        <Header />
+        <About />
+        <Portfolio projects={data.projects} />
+        <Contact />
+        <Footer />
+      </Layout>
+    </>
   );
 };
 
