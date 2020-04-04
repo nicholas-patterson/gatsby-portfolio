8e778f69e65bf012035029f9f51ffd2349e09531
nicholas-patterson <nicholaspatterson36@gmail.com>
2020-04-04 02:26:17 -0400

    :sparkles: adds gatsby config file
    adds gatsby config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,25 +1,55 @@
 module.exports = {
   plugins: [
-    "gatsby-plugin-sass",
-    "gatsby-transformer-sharp",
     "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-background-image",
+    "gatsby-plugin-sass",
+    "gatsby-transformer-json",
+    "gatsby-plugin-smoothscroll",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "Nicholas | Full-Stack Developer",
+        short_name: "Nicholas Codes",
+        background_color: "#333",
+        theme_color: "#4753dd",
+        start_url: "/",
+        icon: "./src/data/images/FinalDesignFadeCopy.png",
+      },
+    },
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      trackingId: "UA-162762200-1",
+      head: false,
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "project",
+        path: "./src/data/",
+      },
+    },
     {
       resolve: "gatsby-plugin-prefetch-google-fonts",
       options: {
         fonts: [
           {
             family: "Montserrat",
-            variants: ["200", "400", "400i", "600", "600i", "700"]
-          }
-        ]
-      }
-    }
+            variants: ["200", "400", "400i", "600", "600i", "700"],
+          },
+        ],
+      },
+    },
   ],
   siteMetadata: {
     title: "Nicholas Codes | Portfolio",
     siteUrl: "https://nicholascodes.com",
     description:
       "I'm Nicholas a 22 year old self-taught Full Stack developer, from Philadelphia",
-    author: "Nicholas Patterson"
-  }
+    author: "Nicholas Patterson",
+    email: "hello@nicholascodes.com",
+    twitter_username: "@Nick_Codes_",
+    site_image: "./src/data/images/FinalDesignFadeCopy.png",
+  },
 };
