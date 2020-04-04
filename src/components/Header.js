55d883e065af6c6bc20093efa2fe870957a7eb85
nicholas-patterson <nicholaspatterson36@gmail.com>
2020-04-04 02:29:23 -0400

    :sparkles: adds header comp and scrollTo plugin
    adds header comp and scrollTo plugin

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,32 @@
 import React from "react";
+import scrollTo from "gatsby-plugin-smoothscroll";
 
 const Header = () => {
-  return <h1>Header Component Here</h1>;
+  return (
+    <div className="header">
+      <div className="header__center">
+        <h3 className="header__greeting">
+          Hello
+          <span role="img" aria-label="Peace Sign">
+            ✌️
+          </span>
+          , <br />
+          I'm <span className="header__greeting--name">Nicholas</span>
+        </h3>
+        <p className="header__para">
+          A <span className="header__para--italic">fullstack</span> web
+          developer
+        </p>
+        <button
+          onClick={() => scrollTo("#about")}
+          className="header__btn"
+          type="button"
+        >
+          About Me
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
