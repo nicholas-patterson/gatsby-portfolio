9780960071845b3692d92da87cb6cce52aa4182d
nicholas-patterson <nicholaspatterson36@gmail.com>
2020-04-04 02:28:17 -0400

    :sparkles: adds graphql query and footer comp
    adds graphql query and footer comp

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { graphql, useStaticQuery } from "gatsby";
+import Img from "gatsby-image";
 
 const Footer = () => {
-  return <h1 style={{}}>Footer component Here</h1>;
+  const data = useStaticQuery(graphql`
+    {
+      image: file(relativePath: { eq: "images/FinalDesigncopy.png" }) {
+        id
+        childImageSharp {
+          fixed(width: 150) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `);
+
+  const date = new Date().getFullYear();
+  return (
+    <div className="footer">
+      <div className="footer-pic-container">
+        <Img className="footer__img" fixed={data.image.childImageSharp.fixed} />
+      </div>
+      <p>Copyright &copy; {date} Nicholas Codes</p>
+    </div>
+  );
 };
 
 export default Footer;
