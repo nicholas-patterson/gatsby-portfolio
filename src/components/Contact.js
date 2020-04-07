8c257dc1810b9aaa0d2e881d08c567996efb5622
nicholas-patterson <nicholaspatterson36@gmail.com>
2020-04-07 00:27:26 -0400

    :rocket: deploying contact form
    deploying contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,5 @@
 import React from "react";
 import { graphql, useStaticQuery } from "gatsby";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faFacebookSquare,
-  faTwitterSquare,
-  faGithubSquare,
-  faLinkedin,
-} from "@fortawesome/free-brands-svg-icons";
 
 const Contact = () => {
   const data = useStaticQuery(graphql`
@@ -22,57 +15,26 @@ const Contact = () => {
   return (
     <div className="contact" id="contact">
       <h3 className="contact__title">Contact Me</h3>
-      <p className="contact__para">Let's talk, Shoot me an email at:</p>
-      <p className="contact__email">
-        <a
-          className="contact__link"
-          href={`mailto:${data.site.siteMetadata.email}`}
-        >
-          {data.site.siteMetadata.email}
-        </a>
-      </p>
-      <ul className="contact__social">
-        <a
-          rel="noopener noreferrer"
-          target="_blank"
-          href="http://www.facebook.com/Patterson.Nicholas1"
-        >
-          <FontAwesomeIcon
-            className="contact__social--icon"
-            icon={faFacebookSquare}
-          />
-        </a>
-        <a
-          rel="noopener noreferrer"
-          target="_blank"
-          href="http://www.twitter.com/Nick_Codes_"
-        >
-          <FontAwesomeIcon
-            className="contact__social--icon"
-            icon={faTwitterSquare}
-          />
-        </a>
-        <a
-          rel="noopener noreferrer"
-          target="_blank"
-          href="http://www.github.com/nicholas-patterson"
-        >
-          <FontAwesomeIcon
-            className="contact__social--icon"
-            icon={faGithubSquare}
-          />
-        </a>
-        <a
-          rel="noopener noreferrer"
-          target="_blank"
-          href="https://www.linkedin.com/in/nicholas-patterson-458593188/"
-        >
-          <FontAwesomeIcon
-            className="contact__social--icon"
-            icon={faLinkedin}
-          />
-        </a>
-      </ul>
+      <div className="contact__form">
+        <form method="POST" data-netlify="true">
+          <div className="field">
+            <input type="text" name="name" id="name" placeholder="Name" />
+          </div>
+          <div className="field">
+            <input type="email" name="email" id="email" placeholder="Email" />
+          </div>
+          <div className="field">
+            <textarea
+              name="message"
+              id="message"
+              placeholder="Message"
+              rows="7"
+            ></textarea>
+          </div>
+          <div className="field" data-netlify-recaptcha="true"></div>
+          <button type="submit">Send It</button>
+        </form>
+      </div>
     </div>
   );
 };
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,12 @@
 import React from "react";
 import { graphql, useStaticQuery } from "gatsby";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebookSquare,
+  faTwitterSquare,
+  faGithubSquare,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
 import Img from "gatsby-image";
 
 const Footer = () => {
@@ -19,6 +26,48 @@ const Footer = () => {
   const date = new Date().getFullYear();
   return (
     <div className="footer">
+      <ul className="contact__social">
+        <a
+          rel="noopener noreferrer"
+          cleartarget="_blank"
+          href="http://www.facebook.com/Patterson.Nicholas1"
+        >
+          <FontAwesomeIcon
+            className="contact__social--icon"
+            icon={faFacebookSquare}
+          />
+        </a>
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          href="http://www.twitter.com/Nick_Codes_"
+        >
+          <FontAwesomeIcon
+            className="contact__social--icon"
+            icon={faTwitterSquare}
+          />
+        </a>
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          href="http://www.github.com/nicholas-patterson"
+        >
+          <FontAwesomeIcon
+            className="contact__social--icon"
+            icon={faGithubSquare}
+          />
+        </a>
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://www.linkedin.com/in/nicholas-patterson-458593188/"
+        >
+          <FontAwesomeIcon
+            className="contact__social--icon"
+            icon={faLinkedin}
+          />
+        </a>
+      </ul>
       <div className="footer-pic-container">
         <Img className="footer__img" fixed={data.image.childImageSharp.fixed} />
       </div>
