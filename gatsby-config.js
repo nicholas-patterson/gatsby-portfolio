3635b3987347ecc09681862d490dbeb01f1e02ea
Nicholas Patterson <nicholaspatterson36@outlook.com>
2021-07-22 22:46:29 -0400

    netlfiy build failed becuase of outdated plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,22 +38,23 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-plugin-prefetch-google-fonts",
+      resolve: "gatsby-plugin-webfonts",
       options: {
-        fonts: [
-          {
-            family: "Montserrat",
-            variants: ["200", "400", "400i", "600", "600i", "700"],
-          },
-        ],
+        fonts: {
+          google: [
+            {
+              family: "Montserrat",
+              variations: ["200", "400", "400i", "600", "600i", "700"],
+            },
+          ],
+        },
       },
     },
   ],
   siteMetadata: {
     title: "Nicholas Codes | Portfolio",
     siteUrl: "https://nicholascodes.com",
-    description:
-      "I'm Nicholas a 22 year old self-taught Full Stack developer, from Philadelphia",
+    description: "I'm Nicholas a Full Stack developer, from Philadelphia",
     author: "Nicholas Patterson",
     email: "nicholaspatterson36@gmail.com",
     twitter_username: "@nicholascodes",
